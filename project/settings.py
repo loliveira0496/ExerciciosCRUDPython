e9514d98ef9a8666e1fac68a6be3e1b49f0e97ef
Lorran Oliveira <lorran.info0496@gmail.com>
2023-10-05 11:54:21 -0300

    Aula 08 de CRUD

--- project/settings.py
+++ project/settings.py
@@ -76,9 +76,18 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sistema',
+        'PORT': '3306',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+        
     }
 }
 
